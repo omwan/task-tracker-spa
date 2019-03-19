f628f58fe652834a3b0356d36275034eb04aac8a
Olivia Wan <wan.o@husky.neu.edu>
2019-03-18 21:41:00 -0400

    login/logout functionality with redux

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,8 +20,9 @@ import _ from "lodash";
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
 import root_init from "./root";
+import store from "./store";
 
 $(() => {
     let node = $('#root')[0];
-    root_init(node);
+    root_init(node, store);
 });
--- assets/js/root.jsx
+++ assets/js/root.jsx
@@ -2,66 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import _ from 'lodash';
 import $ from 'jquery';
+import {Provider} from 'react-redux';
 
-export default function root_init(node) {
-    let prods = window.products;
-    ReactDOM.render(<Root products={prods}/>, node);
+import api from './api';
+
+import Header from './header';
+
+export default function root_init(node, store) {
+    ReactDOM.render(
+        <Provider store={store}>
+            <Root/>
+        </Provider>, node);
 }
 
 class Root extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            loginForm: {
-                username: "",
-                password: ""
-            },
-            session: null
-        };
-    }
-
-    updateLoginForm(data) {
-        let form = _.assign({}, this.state.loginForm, data);
-        this.setState(_.assign({}, this.state, {loginForm: form}))
-    }
-
-    login() {
-        $.ajax("/api/v1/auth", {
-            method: "POST",
-            dataType: "json",
-            contentType: "application/json; charset=UTF-8",
-            data: JSON.stringify(this.state.loginForm),
-            success: function (response) {
-                console.log(response.data);
-            }
-        })
     }
 
     render() {
         return <div>
-            <Header root={this} session={this.state.session}/>
+            <Header/>
         </div>
     }
-}
-
-function Header(props) {
-    let {root, session} = props;
-    let sessionInfo = null;
-
-    if (session == null) {
-        sessionInfo = <div className="form-inline my-2">
-            <input type="username" placeholder="username"
-                   onChange={(ev) => root.updateLoginForm({username: ev.target.value})}/>
-            <input type="password" placeholder="password"
-                   onChange={(ev) => root.updateLoginForm({password: ev.target.value})}/>
-            <button className="btn btn-secondary" onClick={() => root.login()}>Login</button>
-        </div>;
-    } else {
-        sessionInfo = <div className="my-2">
-            {/*<p>Logged in as {session.user_id}</p>*/}
-            <p>Logged in as Olivia</p>
-        </div>
-    }
-
-    return sessionInfo;
 }
