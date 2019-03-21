f2ba10fbfa9448154d71da05a60f3a64402d16c0
Olivia Wan <wan.o@husky.neu.edu>
2019-03-21 16:14:15 -0400

    refactoring api.js to use helpers for ajax calls

--- assets/js/api.js
+++ assets/js/api.js
@@ -2,141 +2,125 @@ import store from './store';
 import $ from "jquery";
 
 class Server {
-    createSession(username, password) {
-        $.ajax("/api/v1/auth", {
-            method: "POST",
+    executeAjax(method, url, data, success, error = function(){}) {
+        $.ajax(url, {
+            method: method,
             dataType: "json",
             contentType: "application/json; charset=UTF-8",
-            data: JSON.stringify({username, password}),
-            success: function (response) {
-                store.dispatch({
-                    type: "NEW_SESSION",
-                    data: response.data
-                });
-            }
-        })
+            data: JSON.stringify(data),
+            success: success,
+            error: error
+        });
+    }
+
+    getData(url, success, error = function(){}) {
+        this.executeAjax("GET", url, "", success, error);
+    }
+
+    postData(url, data, success, error = function(){}) {
+        this.executeAjax("POST", url, data, success, error);
+    }
+
+    putData(url, data, success, error = function(){}) {
+        this.executeAjax("PUT", url, data, success, error);
+    }
+
+    deleteData(url, success, error = function(){}) {
+        this.executeAjax("DELETE", url, "", success, error)
+    }
+
+    createSession(username, password) {
+        let successFunction = function (response) {
+            store.dispatch({
+                type: "NEW_SESSION",
+                data: response.data
+            });
+        };
+        let body = {username, password};
+        this.postData("/api/v1/auth", body, successFunction);
     }
 
     deleteSession() {
-        $.ajax("/api/v1/auth", {
-            method: "DELETE",
-            dataType: "json",
-            contentType: "application/json; charset=UTF-8",
-            data: "",
-            success: function() {
-                store.dispatch({
-                    type: "DELETE_SESSION",
-                });
-                store.dispatch({
-                    type: "USER_LOGGED_OUT",
-                });
-            }
-        })
+        let successFunction = function() {
+            store.dispatch({
+                type: "DELETE_SESSION",
+            });
+            store.dispatch({
+                type: "USER_LOGGED_OUT",
+            });
+        };
+        this.deleteData("/api/v1/auth", successFunction);
     }
 
     fetchUsers() {
-        $.ajax(`/api/v1/users`, {
-            method: "GET",
-            dataType: "json",
-            contentType: "application/json; charset=UTF-8",
-            data: "",
-            success: function (response) {
-                store.dispatch({
-                    type: 'USER_LIST',
-                    data: response.data
-                })
-            }
-        });
+        let successFunction = function (response) {
+            store.dispatch({
+                type: 'USER_LIST',
+                data: response.data
+            });
+        };
+        this.getData("/api/v1/users", successFunction);
     }
 
     fetchUser(id) {
-        $.ajax(`/api/v1/users/${id}`, {
-            method: "GET",
-            dataType: "json",
-            contentType: "application/json; charset=UTF-8",
-            data: "",
-            success: function (response) {
-                store.dispatch({
-                    type: 'USER_DETAIL',
-                    data: response.data
-                })
-            }
-        });
+        let successFunction = function (response) {
+            store.dispatch({
+                type: 'USER_DETAIL',
+                data: response.data
+            });
+        };
+        this.getData(`/api/v1/users/${id}`, successFunction);
     }
 
     fetchTasks() {
-        $.ajax("/api/v1/tasks", {
-            method: "GET",
-            dataType: "json",
-            contentType: "application/json; charset=UTF-8",
-            data: "",
-            success: function (response) {
-                store.dispatch({
-                    type: 'TASK_LIST',
-                    data: response.data
-                })
-            }
-        })
+        let successFunction = function(response) {
+            store.dispatch({
+                type: 'TASK_LIST',
+                data: response.data
+            });
+        };
+        this.getData("/api/v1/tasks", successFunction);
     }
 
     fetchTask(id) {
-        $.ajax(`/api/v1/tasks/${id}`, {
-            method: "GET",
-            dataType: "json",
-            contentType: "application/json; charset=UTF-8",
-            data: "",
-            success: function (response) {
-                store.dispatch({
-                    type: 'TASK_DETAIL',
-                    data: response.data
-                })
-            }
-        });
+        let successFunction = function (response) {
+            store.dispatch({
+                type: 'TASK_DETAIL',
+                data: response.data
+            });
+        };
+        this.getData(`/api/v1/tasks/${id}`, successFunction);
     }
 
     updateTask(id, body) {
-        $.ajax(`/api/v1/tasks/${id}`, {
-            method: "PUT",
-            dataType: "json",
-            contentType: "application/json; charset=UTF-8",
-            data: JSON.stringify(body),
-            success: function (response) {
-                store.dispatch({
-                    type: 'TASK_DETAIL',
-                    data: response.data
-                })
-            }
-        });
+        let successFunction = function (response) {
+            store.dispatch({
+                type: 'TASK_DETAIL',
+                data: response.data
+            });
+        };
+        this.putData(`/api/v1/tasks/${id}`, body, successFunction);
     }
 
     createTask(body) {
-        $.ajax(`/api/v1/tasks`, {
-            method: "POST",
-            dataType: "json",
-            contentType: "application/json; charset=UTF-8",
-            data: JSON.stringify(body),
-            success: function (response) {
-                store.dispatch({
-                    type: 'TASK_DETAIL',
-                    data: response.data
-                })
-            }
-        });
+        let successFunction = function(response) {
+            store.dispatch({
+                type: 'TASK_DETAIL',
+                data: response.data
+            });
+        };
+        this.postData("/api/v1/tasks", body, successFunction);
     }
 
     deleteTask(id) {
-        $.ajax(`/api/v1/tasks/${id}`, {
-            method: "DELETE",
-            dataType: "json",
-            contentType: "application/json; charset=UTF-8",
-            data: "",
-            success: function() {
-                store.dispatch({
-                    type: 'DELETE_TASK',
-                    taskId: id
-                });
-            }
-        })
+        let successFunction = function() {
+            console.log("hey");
+            store.dispatch({
+                type: 'DELETE_TASK',
+                taskId: id
+            });
+        };
+        this.deleteData(`/api/v1/tasks/${id}`, successFunction);
     }
 }
 
